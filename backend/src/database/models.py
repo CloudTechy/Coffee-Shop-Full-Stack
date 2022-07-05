ea6813fe1cfbd0c22779b7599092d345f41dbf47
CloudTechy <onyekeluchukwuebuka@yahoo.ca>
2022-07-05 12:55:38 +0100

    feature: adds local environmentaal variable capability

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -2,8 +2,11 @@ import os
 from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
+from dotenv import dotenv_values
 
-database_filename = "database.db"
+config = dotenv_values(".env")
+
+database_filename = config['sqlite_filename']
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
 
