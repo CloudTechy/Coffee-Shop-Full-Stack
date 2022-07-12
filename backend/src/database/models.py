f516050e5885e473314c0970a5036323155a1828
CloudTechy <onyekeluchukwuebuka@yahoo.ca>
2022-07-12 04:21:10 +0100

    creates the API endpoints with authorization checks

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -8,7 +8,8 @@ config = dotenv_values(".env")
 
 database_filename = config['sqlite_filename']
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+database_path = "sqlite:///{}".format(
+    os.path.join(project_dir, database_filename))
 
 db = SQLAlchemy()
 
@@ -29,7 +30,8 @@ def setup_db(app):
 db_drop_and_create_all()
     drops the database tables and starts fresh
     can be used to initialize a clean database
-    !!NOTE you can change the database_filename variable to have multiple verisons of a database
+    !!NOTE you can change the database_filename variable to have multiple
+    verisons of a database
 '''
 
 
@@ -41,10 +43,9 @@ def db_drop_and_create_all():
         title='water',
         recipe='[{"name": "water", "color": "blue", "parts": 1}]'
     )
-
-
     drink.insert()
 # ROUTES
+
 
 '''
 Drink
@@ -58,7 +59,8 @@ class Drink(db.Model):
     # String Title
     title = Column(String(80), unique=True)
     # the ingredients blob - this stores a lazy json blob
-    # the required datatype is [{'color': string, 'name':string, 'parts':number}]
+    # the required datatype is
+    # [{'color': string, 'name':string, 'parts':number}]
     recipe = Column(String(180), nullable=False)
 
     '''
@@ -68,7 +70,9 @@ class Drink(db.Model):
 
     def short(self):
         print(json.loads(self.recipe))
-        short_recipe = [{'color': r['color'], 'parts': r['parts']} for r in json.loads(self.recipe)]
+        short_recipe = [
+            {'color': r['color'], 'parts': r['parts']}
+            for r in json.loads(self.recipe)]
         return {
             'id': self.id,
             'title': self.title,
